9fb275ee0c0c2e245cbda5f79d7023c6ec8c5048
zy_zero <jiyoungjang0307@gmail.com>
2023-10-30 15:53:31 +0900

    view controller

--- src/main/java/com/example/demo/viewController/ViewController.java
+++ src/main/java/com/example/demo/viewController/ViewController.java
@@ -1,10 +1,12 @@
 package com.example.demo.viewController;
 
+import ch.qos.logback.core.net.server.ServerRunner;
 import com.example.demo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/view")
@@ -23,5 +25,22 @@ public class ViewController {
     student.setName("testName");
     model.addAttribute("student", student);
     return "obj";
+  }
+
+  @GetMapping("/link")
+  //  입력받을 2개의 매개변수를 던져서 보이게 할 예정이니 Model에 담아줌
+  public String method3(Model model) {
+    model.addAttribute("param1", 3);
+    model.addAttribute("param2", 8);
+    return "link";
+  }
+
+  @GetMapping("/param")
+//  입력 받을 2개의 매개변수를 던져서 보이게 할 예정이니 Model에 담아줌
+  public String method4(@RequestParam Integer param1,
+                        @RequestParam Integer param2,
+                        Model model) {
+    model.addAttribute("result", param1 + param2);
+    return "param";
   }
 }
