261f84e40e90e6c63872c6f6ec1422a1560392cd
zy_zero <jiyoungjang0307@gmail.com>
2023-10-31 15:04:52 +0900

    apply mybatis

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,11 +3,13 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import com.example.demo.model.PostDTO;
 import com.example.demo.repository.PostRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class PostService {
   @Autowired
@@ -25,6 +27,7 @@ public class PostService {
   public Post updatePost(int postId, PostDTO postDTO) {
     Post orgPost = postRepository.selectPostById(postId);
     orgPost.setLikes(postDTO.getLikes());
+    System.out.printf("orgPost.likes", orgPost.getLikes());
     postRepository.updatePost(orgPost);
     return orgPost;
   }
--- src/main/java/com/example/demo/webController/WebPostController.java
+++ src/main/java/com/example/demo/webController/WebPostController.java
@@ -13,58 +13,52 @@ import java.util.List;
 @Controller
 @RequestMapping("/posts")
 public class WebPostController {
-  private final PostService postService;
+    private final PostService postService;
 
-  @Autowired
-  public WebPostController(PostService postService) {
-    this.postService = postService;
-  }
+    @Autowired
+    public WebPostController(PostService postService) {
+        this.postService = postService;
+    }
 
-  @GetMapping
-  public String getAllPosts(Model model){
-    List<Post> allPosts = postService.getAllPosts();
-    model.addAttribute("posts", allPosts);
-    return "post/postList";
-  }
+    @GetMapping
+    public String getAllPosts(Model model) {
+        List<Post> allPosts = postService.getAllPosts();
+        model.addAttribute("posts", allPosts);
+        return "post/postList";
+    }
 
-  @GetMapping("/addPost")
-  public String addPost(){
-    return "post/postForm";
-  }
+    @GetMapping("/addPost")
+    public String addPost() {
+        return "post/postForm";
+    }
 
-  @PostMapping("/addPost")
-  public String addPost(@ModelAttribute Post post){
-    postService.setPost(post);
-    return "redirect:/posts";
-  }
+    @PostMapping("/addPost")
+    public String addPost(@ModelAttribute Post post) {
+        postService.setPost(post);
+        return "redirect:/posts";
+    }
 
-  @GetMapping("/postInfo/{postId}")
-  public String getPostInfo(@PathVariable int postId,
-                            Model model){
-    Post post = postService.getPostById(postId);
-    model.addAttribute("post", post);
-    return "post/postInfo";
-  }
+    @GetMapping("/postInfo/{postId}")
+    public String getPostInfo(@PathVariable int postId,
+                              Model model) {
+        Post post = postService.getPostById(postId);
+        model.addAttribute("post", post);
+        return "post/postInfo";
+    }
 
-  @GetMapping("/updatePost/{postId}")
-  public String updatePost(@PathVariable int postId,
-                           Model model){
-    Post post = postService.getPostById(postId);
-    model.addAttribute("post", post);
-    return "post/postUpdateForm";
-  }
+    @GetMapping("/updatePost/{postId}")
+    public String updatePost(@PathVariable int postId,
+                             Model model) {
+        Post post = postService.getPostById(postId);
+        model.addAttribute("post", post);
+        return "post/postUpdateForm";
+    }
 
-  @PostMapping("/updatePost/{postId}")
-  public String updatePost(@PathVariable int postId,
-                           @ModelAttribute PostDTO postDto){
-    Post post = postService.getPostById(postId);
-    post.setLikes(postDto.getLikes());
-    return "redirect:/posts";
-  }
 
-  @GetMapping("/deletePost/{postId}")
-  public String deletePost(@PathVariable int postId){
-    postService.removePost(postId);
-    return "redirect:/posts";
-  }
+
+    @GetMapping("/deletePost/{postId}")
+    public String deletePost(@PathVariable int postId) {
+        postService.removePost(postId);
+        return "redirect:/posts";
+    }
 }
